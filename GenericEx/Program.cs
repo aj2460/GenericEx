06d54eb625ee2eb303f89859b334dea0ca3af56f
Anand Janardhanan <ajkadakkal@gmail.com>
2020-11-12 13:29:51 +0530

    Using generic on class type

--- GenericEx/Program.cs
+++ GenericEx/Program.cs
@@ -8,13 +8,26 @@ namespace GenericEx
         {
             string  ans =  Calculator.AreTheyEqual<int>(10, 20)?"True":"False";
             Console.WriteLine(ans);
-            
+
+            string ans1 = Calculator1<int>.AreTheyEqual(10, 10) ? "True" : "False";
+            Console.WriteLine(ans1);
         }
     }
 
     public class Calculator
     {
         public static bool AreTheyEqual<T>(T Value1, T Value2)
+        {
+            return Value1.Equals(Value2);
+        }
+
+    }
+
+    //Making class genetric
+
+    public class Calculator1<T>
+    {
+        public static bool AreTheyEqual(T Value1, T Value2)
         {
             return Value1.Equals(Value2);
         }
