0666b9254dccd764f0e9eac88bb24972beb0c2ea
Anand Janardhanan <ajkadakkal@gmail.com>
2020-11-12 13:15:49 +0530

    Add project files.

---
+++ GenericEx/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  ans =  Calculator.AreTheyEqual<int>(10, 20)?"True":"False";
+            Console.WriteLine(ans);
+            
+        }
+    }
+
+    public class Calculator
+    {
+        public static bool AreTheyEqual<T>(T Value1, T Value2)
+        {
+            return Value1.Equals(Value2);
+        }
+
+    }
+}
